c6933c9b2a79666e248b26f92b66484653518177
Michael <michaelcloran2010@gmail.com>
2024-05-04 17:28:21 +0100

    feat: added form validation on edit_task

--- static/js/tasks.js
+++ static/js/tasks.js
@@ -10,18 +10,16 @@ const deleteTaskModal = new bootstrap.Modal(document.getElementById("deleteTaskM
 
 for (let button of editTasksButtons){
 button.addEventListener("click", (e) => {
-  console.log("TP1: here");
+
   let taskId = e.target.getAttribute("data-task_id");
   //let taskContent = document.getElementById(`task${taskId}`).innerText;
 
-  console.log("TP1:taskid:"+taskId); //+" taskContent:"+taskContent);
     editTaskModal.show();
 
     let parent = e.target.parentElement;
-    console.log(parent);
 
     let category = parent.getElementsByClassName("task-category")[0].innerText;
-    console.log("cat:"+category);
+
     let category_el = document.getElementById("id_category_id");
     for(let i=0; i< category_el.options.length; i++){
       if(category_el.options[i].text === category){
@@ -30,41 +28,58 @@ button.addEventListener("click", (e) => {
     }
 
     let task_name_el = parent.getElementsByClassName('task-name');
-    console.log(task_name_el);
-    console.log(task_name_el[0].innerText);
+
     document.getElementById("id_task_name").value = task_name_el[0].innerText;
 
     let task_description_el = parent.getElementsByClassName('task-description');
-    console.log(task_description_el);
-    console.log(task_description_el[0].innerText);
+
     document.getElementById("id_task_description").value = task_description_el[0].innerText;
 
     let task_date_el = parent.getElementsByClassName('task-date');
-    console.log(task_date_el);
-    console.log(task_date_el[0].innerText);
+
     document.getElementById("id_date").value = task_date_el[0].innerText;
 
     let task_start_el = parent.getElementsByClassName('task-start');
-    console.log(task_start_el);
-    console.log(task_start_el[0].innerText);
+
     document.getElementById("id_start_time").value = task_start_el[0].innerText;
 
 
     let task_end_el = parent.getElementsByClassName('task-end');
-    console.log(task_end_el);
-    console.log(task_end_el[0].innerText);
+
     document.getElementById("id_end_time").value = task_end_el[0].innerText;
 
-
-    console.log("URL:"+window.location.href);
-
     editConfirm.addEventListener("click", (e) => {
-      editTaskForm.action=`edit_task/${taskId}`;
 
-      editTaskForm.submit();
+      if (!editTaskForm.checkValidity()) {
+
+        e.preventDefault();
+        e.stopPropagation();
+
+        field = Array.from(editTaskForm.elements);
+
+        // reset fields
+        field.forEach(i => {
+          i.setCustomValidity('');
+          i.classList.remove('invalid');
+        });
+
+        // apply invalid class
+        field.forEach(i => {
+          if (!i.checkValidity()) {
+            // field is invalid - add class
+            i.classList.add('invalid');
+          }
+        });
+
+      }else{
+
+        editTaskForm.action=`edit_task/${taskId}`;
+
+        editTaskForm.submit();
+      }
     });
     editCancel.addEventListener("click", (e) => {
-      console.log("cancel task clicked");
+
       editTaskForm.submit();
     });
 });
@@ -76,24 +91,19 @@ for (let button of deleteTaskButtons) {
     let task_date_el = parent.getElementsByClassName('task-date');
 
     let view_date = task_date_el[0].innerText;
-    console.log("TP view_date: "+view_date);
 
     let todays_date = new Date();
-    console.log(todays_date);
     let year = todays_date.getFullYear();
     let month = String(todays_date.getMonth() + 1).padStart(2, '0');
     let day = String(todays_date.getDate()).padStart(2, '0');
-    console.log("TP year:"+year+"-"+month+"-"+day);
 
     todays_date = year+"-"+month+"-"+day;
 
     let taskId = e.target.getAttribute("data-task_id");
 
     if(todays_date === view_date){
-      console.log("TP hurray!!");
       deleteTaskConfirm.href = `delete_task/${taskId}`;
     }else{
-      console.log("TP dates not equal!!");
       let index_of_date = window.location.href.indexOf(view_date);
       if(index_of_date == -1){
         deleteTaskConfirm.href = `${view_date}/delete_task/${taskId}`;
---
+++ staticfiles/js/category.js
@@ -0,0 +1,64 @@
+
+const categoryForm = document.getElementById("categoryForm");
+const submitButton = document.getElementById("submitButton");
+
+const editModal = new bootstrap.Modal(document.getElementById("editModal"));
+const editButtons = document.getElementsByClassName("btn-edit");
+const editConfirm = document.getElementById("update");
+const editCancel = document.getElementById("cancel");
+const categoryText = document.getElementById("id_category_name");
+
+const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+const deleteButtons = document.getElementsByClassName("btn-delete");
+const deleteConfirm = document.getElementById("deleteConfirm");
+
+
+/**
+* Initializes edit functionality for the provided edit buttons.
+*
+* For each button in the `editButtons` collection:
+* - Retrieves the associated comment's ID upon click.
+* - Fetches the content of the corresponding comment.
+* - Populates the `commentText` input/textarea with the comment's content for editing.
+* - Updates the submit button's text to "Update".
+* - Sets the form's action attribute to the `edit_comment/{commentId}` endpoint.
+*/
+for (let button of editButtons) {
+button.addEventListener("click", (e) => {
+
+    let categoryId = e.target.getAttribute("data-category_id");
+
+    let categoryContent = document.getElementById(`category${categoryId}`).innerText;
+    editModal.show();
+
+    categoryText.value = categoryContent;
+
+    editConfirm.addEventListener("click", (e) => {
+      editForm.action=`edit_category/${categoryId}`;
+
+      editForm.submit();
+    });
+    editCancel.addEventListener("click", (e) => {
+      console.log("cancel clicked");
+      editForm.submit();
+    });
+  });
+}
+
+/**
+* Initializes deletion functionality for the provided delete buttons.
+*
+* For each button in the `deleteButtons` collection:
+* - Retrieves the associated comment's ID upon click.
+* - Updates the `deleteConfirm` link's href to point to the
+* deletion endpoint for the specific comment.
+* - Displays a confirmation modal (`deleteModal`) to prompt
+* the user for confirmation before deletion.
+*/
+for (let button of deleteButtons) {
+    button.addEventListener("click", (e) => {
+      let categoryId = e.target.getAttribute("data-category_id");
+      deleteConfirm.href = `delete_category/${categoryId}`;
+      deleteModal.show();
+    });
+}
