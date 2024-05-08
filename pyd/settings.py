e076cc168d616c4d48f293813a8cfe7b44febb04
Michael <michaelcloran2010@gmail.com>
2024-05-08 15:29:50 +0100

    feat: styled stats form

--- pyd/settings.py
+++ pyd/settings.py
@@ -34,7 +34,7 @@ AUTHENTICATION_BACKENDS = (
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
